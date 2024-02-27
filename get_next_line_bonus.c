feab506d7ff34f1e05db2d061409913779a050ec
Enzo Degraeve <enzodeg40@gmail.com>
2024-02-27 15:58:24 +0100

    Import from intra of 42

---
+++ get_next_line_bonus.c
@@ -0,0 +1,150 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edegraev <edegraev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/27 14:16:21 by edegraev          #+#    #+#             */
+/*   Updated: 2023/12/07 03:46:57 by edegraev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_get_line(char *stash)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (!stash[i])
+		return (NULL);
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	str = (char *)malloc(sizeof(char) * (i + 2));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+	{
+		str[i] = stash[i];
+		i++;
+	}
+	if (stash[i] == '\n')
+	{
+		str[i] = stash[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_new_stash(char *stash)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (!stash[i])
+	{
+		free(stash);
+		return (NULL);
+	}
+	str = (char *)malloc(sizeof(char) * (ft_strlen(stash) - i + 1));
+	if (!str)
+		return (NULL);
+	i++;
+	j = 0;
+	while (stash[i])
+		str[j++] = stash[i++];
+	str[j] = '\0';
+	free(stash);
+	return (str);
+}
+
+char	*ft_read_to_stash(int fd, char *stash)
+{
+	char	*buff;
+	int		rd_bytes;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	rd_bytes = 1;
+	while (!ft_strchr(stash, '\n') && rd_bytes != 0)
+	{
+		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		if (rd_bytes == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[rd_bytes] = '\0';
+		stash = ft_strjoin(stash, buff);
+	}
+	free(buff);
+	return (stash);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*stash[4096];
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	stash[fd] = ft_read_to_stash(fd, stash[fd]);
+	if (!stash[fd])
+		return (NULL);
+	line = ft_get_line(stash[fd]);
+	stash[fd] = ft_new_stash(stash[fd]);
+	return (line);
+}
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main(void)
+// {
+// 	int		fd1;
+// 	int		fd2;
+// 	int		fd3;
+// 	char	*line;
+
+// 	fd1 = open("text.txt", O_RDONLY);
+// 	fd2 = open("text2.txt", O_RDONLY);
+// 	fd3 = open("text3.txt", O_RDONLY);
+// 	if (fd1 == -1)
+// 	{
+// 		printf("Error cannot open file\n");
+// 		return (0);
+// 	}
+// 	if (fd2 == -1)
+// 	{
+// 		printf("Error cannot open file\n");
+// 		return (0);
+// 	}
+// 	if (fd3 == -1)
+// 	{
+// 		printf("Error cannot open file\n");
+// 		return (0);
+// 	}
+// 	// while ((line = get_next_line(fd)) != NULL)
+// 	// {
+// 	// 	printf("%s", line);
+// 	// 	free(line);
+// 	// }
+// 	line = get_next_line(fd1);
+// 	printf("%s", line);
+// 	free(line);
+// 	line = get_next_line(fd2);
+// 	printf("%s", line);
+// 	free(line);
+// 	line = get_next_line(fd3);
+// 	printf("%s", line);
+// 	free(line);
+// 	return 0;
+// }
---
+++ get_next_line_bonus.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edegraev <edegraev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/07 03:45:05 by edegraev          #+#    #+#             */
+/*   Updated: 2023/12/07 03:45:05 by edegraev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 64
+# elif BUFFER_SIZE <= 0
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 0
+# endif
+
+char	*ft_get_line(char *stash);
+char	*ft_new_stash(char *stash);
+char	*ft_read_to_stash(int fd, char *stash);
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *stash, char *buff);
+char	*ft_strchr(char *s, int c);
+
+#endif
